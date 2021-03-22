a7df92f2bdf1a3ac2dcb6e927a23739baac9bf66
vitaetehari <79825739+vitaetehari@users.noreply.github.com>
2021-03-22 12:23:38 +0300

    Update task1.pas

--- lw2/task1.pas
+++ lw2/task1.pas
@@ -1,6 +1,12 @@
 PROGRAM PrintHello(INPUT, OUTPUT);
+USES DOS;
 BEGIN {PrintHello}
-  WRITELN(‘Content-Type: text/plain’);
+  WRITELN('Content-Type: text/html');
   WRITELN;
-  WRITELN(‘Hello world!’);
+  WRITELN('<h1>PASCAL</h1>');
+  WRITELN('<p> REQUEST_METHOD: ', GetEnv('REQUEST_METHOD'), '</p>');
+  WRITELN('<p> QUERY_STRING: ', GetEnv('QUERY_STRING'), '</p>');
+  WRITELN('<p> CONTENT_LENGTH: ', GetEnv('CONTENT_LENGTH'), '</p>');
+  WRITELN('<p> HTTP_USER_AGENT: ', GetEnv('HTTP_USER_AGENT'), '</p>');
+  WRITELN('<p> HTTP_HOST: ', GetEnv('HTTP_HOST'), '</p>');
 END. {PrintHello}

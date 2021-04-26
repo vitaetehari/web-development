f704362eecb8deff74901b43e0678f8350927e6f
vitaetehari <79825739+vitaetehari@users.noreply.github.com>
2021-04-26 08:24:13 +0300

    Update task1.pas

--- lw2/task1.pas
+++ lw2/task1.pas
@@ -1,12 +1,11 @@
-PROGRAM PrintHello(INPUT, OUTPUT);
-USES DOS;
-BEGIN {PrintHello}
-  WRITELN('Content-Type: text/html');
+PROGRAM CgiTest(INPUT, OUTPUT);
+USES
+  DOS;
+BEGIN {CgiTest}
   WRITELN;
-  WRITELN('<h1>PASCAL</h1>');
-  WRITELN('<p> REQUEST_METHOD: ', GetEnv('REQUEST_METHOD'), '</p>');
-  WRITELN('<p> QUERY_STRING: ', GetEnv('QUERY_STRING'), '</p>');
-  WRITELN('<p> CONTENT_LENGTH: ', GetEnv('CONTENT_LENGTH'), '</p>');
-  WRITELN('<p> HTTP_USER_AGENT: ', GetEnv('HTTP_USER_AGENT'), '</p>');
-  WRITELN('<p> HTTP_HOST: ', GetEnv('HTTP_HOST'), '</p>');
-END. {PrintHello}
+  WRITELN('Request Method: ', GetEnv('REQUEST_METHOD'));
+  WRITELN('Query: ', GetEnv('QUERY_STRING'));
+  WRITELN('Content Length: ', GetEnv('CONTENT_LENGTH'));
+  WRITELN('User Agent: ', GetEnv('HTTP_USER_AGENT'));
+  WRITELN('Host: ', GetEnv('HTTP_HOST'))
+END. {CgiTest}

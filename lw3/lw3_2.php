3071b334b31acfcbc8fb53454cc0f7835f0efa39
vitaetehari <79825739+vitaetehari@users.noreply.github.com>
2021-05-14 23:30:16 +0300

    Create lw3_2.php

---
+++ lw3/lw3_2.php
@@ -0,0 +1,7 @@
+<?php
+if (!isset($_GET['identifier'])) die("Параметр identifier не задан!");
+$string = $_GET['identifier'];
+if (empty($string)) die("No<br />Идентификатор не может быть пустой строкой!");
+if (ctype_digit($string[0])) die("No<br />Идентификатор не может начинаться с цифры!");
+if (!ctype_alnum($string)) die("No<br />Идентификатор может состоять только из латинских букв и цифр!");
+echo "Yes<br />Введённая строка является идентификатором.";

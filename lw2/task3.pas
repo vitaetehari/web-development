daffa8aad3ca36576d6466f905739a3c400096a6
vitaetehari <79825739+vitaetehari@users.noreply.github.com>
2021-05-14 20:36:59 +0300

    Update task3.pas

--- lw2/task3.pas
+++ lw2/task3.pas
@@ -1,6 +1,21 @@
-PROGRAM PrintHello(INPUT, OUTPUT);
-BEGIN {PrintHello}
-  WRITELN(‘Content-Type: text/plain’);
+PROGRAM Hello(INPUT, OUTPUT);
+USES
+  DOS;
+VAR
+  Query, QueryBegining: STRING;
+  FirstSeparatorPosition: INTEGER;
+BEGIN {Hello}
   WRITELN;
-  WRITELN(‘Hello world!’);
-END. {PrintHello}
+  Query := GetEnv('QUERY_STRING');
+  FirstSeparatorPosition := Pos('&', Query);
+  QueryBegining := Copy(Query, 1, 5);
+  IF QueryBegining <> 'name='
+  THEN
+    WRITELN('Hello Anonymous!')
+  ELSE
+    IF FirstSeparatorPosition <> 0
+    THEN
+      WRITELN('Hello dear ', Copy(Query, 6, FirstSeparatorPosition - 6), '!')
+    ELSE
+      WRITELN('Hello dear ', Copy(Query, 6, Length(Query)), '!');
+END. {Hello}
